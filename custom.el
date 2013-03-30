32a9ba91ef581543dfa2ab94afc71ce449ec2f3a
startling <tdixon51793@gmail.com>
2013-03-30 00:50:44 -0400

    Okay, keep the firebelly hash in custom.el

---
+++ custom.el
@@ -0,0 +1,12 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("e9c17fad877d0ffbd0d8faab3ebaa40425ad4833e4bcf83bd600c56c39a9990c" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
